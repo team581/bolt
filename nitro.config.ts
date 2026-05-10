fc6692c699f9a5303f5d4ad9fbe327babf71c95f
Jonah Snider <jonah@jonahsnider.com>
2026-05-09 17:50:39 -0700

    Include source maps in Sentry
    Signed-off-by: Jonah Snider <jonah@jonahsnider.com>

--- nitro.config.ts
+++ nitro.config.ts
@@ -3,6 +3,7 @@ import { juniorNitro } from "@sentry/junior/nitro";
 
 export default defineConfig({
   preset: "node-server",
+  sourcemap: true,
   modules: [
     juniorNitro({
       pluginPackages: ["@sentry/junior-github"],
