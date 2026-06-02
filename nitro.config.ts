f57318f0ca33668814e41bfee1e98d6efecc7ff5
Jonah Snider <jonah@jonahsnider.com>
2026-06-01 23:30:16 -0700

    Add Junior web dashboard
    Signed-off-by: Jonah Snider <jonah@jonahsnider.com>

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from "nitro";
+import { juniorDashboardNitro } from "@sentry/junior-dashboard/nitro";
 import { juniorNitro } from "@sentry/junior/nitro";
 
 export default defineConfig({
 	preset: "node-server",
 	sourcemap: true,
 	modules: [
+		juniorDashboardNitro({
+			allowedGoogleDomains: ["team581.com"],
+		}),
 		juniorNitro({
 			plugins: {
 				packages: ["@sentry/junior-github"],
