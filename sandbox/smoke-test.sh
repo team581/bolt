85b1114094a560183051a531b411161df812c65b
Jonah Snider <jonah@jonahsnider.com>
2026-08-05 22:54:32 -0700

    Install gcloud in sandbox container
    Signed-off-by: Jonah Snider <jonah@jonahsnider.com>

--- sandbox/smoke-test.sh
+++ sandbox/smoke-test.sh
@@ -7,7 +7,9 @@ test -x "$JAVA_HOME/bin/java"
 test "$GRADLE_USER_HOME" = /workspace/.gradle
 test "$GRADLE_RO_DEP_CACHE" = /opt/bolt/gradle-dependencies
 git --version
+gcloud --version
 gh --version
+gsutil version
 node --version
 test -d /opt/bolt/repositories/offseason-2026/.git
 test -d /opt/bolt/gradle-dependencies/modules-2
