3d4f2fab104e7e243a67e90246a1e8b8e707516b
Jonah Snider <jonah@jonahsnider.com>
2026-08-19 18:26:23 +0000

    Use PostgresStoreVNext for Mastra storage
    Signed-off-by: Jonah Snider <jonah@jonahsnider.com>

--- src/mastra/storage.ts
+++ src/mastra/storage.ts
@@ -1,7 +1,8 @@
-import { PostgresStore } from "@mastra/pg";
+import { PostgresStoreVNext } from "@mastra/pg";
 import { config } from "../config.ts";
 
-export const storage = new PostgresStore({
+export const storage = new PostgresStoreVNext({
 	id: "bolt-postgres",
 	connectionString: config.DATABASE_URL,
+	observability: { connectionString: config.DATABASE_URL },
 });
